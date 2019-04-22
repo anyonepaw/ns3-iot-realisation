c3c9d956daba25437327e0344c922a7ed4d81f5f
Anzhelika Pokhodun <an.pokhodun@gmail.com>
2019-04-22 14:14:21 +0300

    Dummy for iot-application. Task for Ilya

--- src/internet-of-things/model/iot-node-application.cc
+++ src/internet-of-things/model/iot-node-application.cc
@@ -1,5 +1,348 @@
 //
-// Created by anyonepaw on 2019-04-20.
+// Created by anyonepaw on 2019-03-26.
 //
 
 #include "iot-node-application.h"
+
+#include <algorithm>
+#include "ns3/application.h"
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+#include "ns3/traced-callback.h"
+#include "ns3/address.h"
+
+#include "ns3/internet-module.h"
+#include "ns3/internet-apps-module.h"
+
+#include "ns3/boolean.h"
+#include "ns3/address-utils.h"
+
+namespace ns3 {
+
+    NS_LOG_COMPONENT_DEFINE ("IotNode");
+
+    NS_OBJECT_ENSURE_REGISTERED (IotNode);
+
+    TypeId
+    IotNode::GetTypeId(void) {
+        static TypeId tid = TypeId("ns3::IotNode")
+                .SetParent<Application>()
+                .SetGroupName("Applications")
+                .AddConstructor<IotNode>()
+                .AddAttribute("Local",
+                              "The Address on which to Bind the rx socket.",
+                              AddressValue(),
+                              MakeAddressAccessor(&IotNode::m_local),
+                              MakeAddressChecker())
+                .AddAttribute("Protocol",
+                              "The type id of the protocol to use for the rx socket.",
+                              TypeIdValue(UdpSocketFactory::GetTypeId()),
+                              MakeTypeIdAccessor(&IotNode::m_tid),
+                              MakeTypeIdChecker())
+                .AddTraceSource("Rx",
+                                "A packet has been received",
+                                MakeTraceSourceAccessor(&IotNode::m_rxTrace),
+                                "ns3::Packet::AddressTracedCallback");
+        return tid;
+    }
+
+    IotNode::IotNode(void) : m_commPort(5555), m_secondsPerMin(60) {
+        NS_LOG_FUNCTION (this);
+        m_socket = 0;
+        m_gatewayNodeId = 0;
+        m_gatewayAddress = Ipv4Address("::1");
+        m_numberOfPeers = m_peersAddresses.size();
+
+    }
+
+
+    IotNode::~IotNode() {
+        NS_LOG_FUNCTION (this);
+    }
+
+    Ptr<Socket>
+    IotNode::GetListeningSocket(void) const {
+        NS_LOG_FUNCTION (this);
+        return m_socket;
+    }
+
+    std::vector<Ipv4Address>
+    IotNode::GetPeersAddresses(void) const {
+        NS_LOG_FUNCTION (this);
+        return m_peersAddresses;
+    }
+
+
+    void
+    IotNode::SetPeersAddresses(const std::vector<Ipv4Address> &peers) {
+        NS_LOG_FUNCTION (this);
+        m_peersAddresses = peers;
+        m_numberOfPeers = m_peersAddresses.size();
+    }
+
+    void
+    IotNode::AddPeer(Ipv4Address newPeer) {
+        NS_LOG_FUNCTION (this);
+        m_peersAddresses.push_back(newPeer);
+        m_numberOfPeers = m_peersAddresses.size();
+    }
+
+    void
+    IotNode::SetPeersDownloadSpeeds(const std::map<Ipv4Address, double> &peersDownloadSpeeds) {
+        NS_LOG_FUNCTION (this);
+        m_peersDownloadSpeeds = peersDownloadSpeeds;
+    }
+
+    void
+    IotNode::SetPeersUploadSpeeds(const std::map<Ipv4Address, double> &peersUploadSpeeds) {
+        NS_LOG_FUNCTION (this);
+        m_peersUploadSpeeds = peersUploadSpeeds;
+    }
+
+    void
+    IotNode::SetNodeInternetSpeeds(const nodeInternetSpeeds &internetSpeeds) {
+        NS_LOG_FUNCTION (this);
+
+        m_downloadSpeed = internetSpeeds.downloadSpeed * 1000000 / 8;
+        m_uploadSpeed = internetSpeeds.uploadSpeed * 1000000 / 8;
+    }
+
+    void
+    IotNode::SetNodeStats(nodeStatistics *nodeStats) {
+        NS_LOG_FUNCTION (this);
+        m_nodeStats = nodeStats;
+    }
+
+    void
+    IotNode::SetProtocolType(enum ProtocolType protocolType) {
+        NS_LOG_FUNCTION (this);
+        m_protocolType = protocolType;
+    }
+
+    void
+    IotNode::DoDispose(void) {
+        NS_LOG_FUNCTION (this);
+        m_socket = 0;
+
+        // chain up
+        Application::DoDispose();
+    }
+
+// Application Methods
+    void
+    IotNode::StartApplication()    // Called at time specified by Start
+    {
+        NS_LOG_FUNCTION (this);
+
+        srand(time(NULL) + GetNode()->GetId());
+        NS_LOG_INFO ("Node " << GetNode()->GetId() << ": download speed = " << m_downloadSpeed << " B/s");
+        NS_LOG_INFO ("Node " << GetNode()->GetId() << ": upload speed = " << m_uploadSpeed << " B/s");
+        NS_LOG_INFO ("Node " << GetNode()->GetId() << ": m_numberOfPeers = " << m_numberOfPeers);
+        NS_LOG_WARN ("Node " << GetNode()->GetId() << ": m_protocolType = " << getProtocolType(m_protocolType));
+
+        NS_LOG_INFO ("Node " << GetNode()->GetId() << ": Gateway node = " << m_gatewayNodeId);
+        NS_LOG_INFO ("Node " << GetNode()->GetId() << ": My peers are");
+
+        for (auto it = m_peersAddresses.begin(); it != m_peersAddresses.end(); it++)
+            NS_LOG_INFO("\t" << *it);
+
+        double currentMax = 0;
+
+        if (!m_socket) {
+            m_socket = Socket::CreateSocket(GetNode(), m_tid);
+            m_socket->Bind(m_local);
+            m_socket->Listen();
+            m_socket->ShutdownSend();
+            if (addressUtils::IsMulticast(m_local)) {
+                Ptr<UdpSocket> udpSocket = DynamicCast<UdpSocket>(m_socket);
+                if (udpSocket) {
+                    // equivalent to setsockopt (MCAST_JOIN_GROUP)
+                    udpSocket->MulticastJoinGroup(0, m_local);
+                } else {
+                    NS_FATAL_ERROR ("Error: joining multicast on a non-UDP socket");
+                }
+            }
+        }
+
+        m_socket->SetRecvCallback(MakeCallback(&IotNode::HandleRead, this));
+        m_socket->SetAcceptCallback(
+                MakeNullCallback<bool, Ptr<Socket>, const Address &>(),
+                MakeCallback(&IotNode::HandleAccept, this));
+        m_socket->SetCloseCallbacks(
+                MakeCallback(&IotNode::HandlePeerClose, this),
+                MakeCallback(&IotNode::HandlePeerError, this));
+
+        NS_LOG_DEBUG ("Node " << GetNode()->GetId() << ": Before creating sockets");
+        for (std::vector<Ipv4Address>::const_iterator i = m_peersAddresses.begin(); i != m_peersAddresses.end(); ++i) {
+            m_peersSockets[*i] = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+//            m_peersSockets[*i]->Connect (Inet6SocketAddress (*i, m_commPort));
+        }
+        NS_LOG_DEBUG ("Node " << GetNode()->GetId() << ": After creating sockets");
+        m_nodeStats->nodeId = GetNode()->GetId();
+        m_nodeStats->connections = m_peersAddresses.size();
+    }
+
+    void
+    IotNode::StopApplication()     // Called at time specified by Stop
+    {
+        NS_LOG_FUNCTION (this);
+
+        for (std::vector<Ipv4Address>::iterator i = m_peersAddresses.begin();
+             i != m_peersAddresses.end(); ++i) //close the outgoing sockets
+        {
+            m_peersSockets[*i]->Close();
+        }
+
+
+        if (m_socket) {
+            m_socket->Close();
+            m_socket->SetRecvCallback(MakeNullCallback<void, Ptr<Socket> >());
+        }
+
+        NS_LOG_WARN ("\n\nIoT Sensor Node " << GetNode()->GetId() << ":");
+
+    }
+
+    void
+    IotNode::HandleRead(Ptr<Socket> socket) {
+        NS_LOG_FUNCTION (this << socket);
+        Ptr<Packet> packet;
+        Address from;
+
+        while ((packet = socket->RecvFrom(from))) {
+            if (packet->GetSize() == 0) { //EOF
+                break;
+            }
+
+            if (Inet6SocketAddress::IsMatchingType(from)) {
+                /**
+                 * We may receive more than one packets simultaneously on the socket,
+                 * so we have to parse each one of them.
+                 */
+                std::string delimiter = "#";
+                std::string parsedPacket;
+                size_t pos = 0;
+                char *packetInfo = new char[packet->GetSize() + 1];
+                std::ostringstream totalStream;
+
+                packet->CopyData(reinterpret_cast<uint8_t *>(packetInfo), packet->GetSize());
+                packetInfo[packet->GetSize()] = '\0'; // ensure that it is null terminated to avoid bugs
+
+                /**
+                 * Add the buffered data to complete the packet
+                 */
+//                totalStream << m_bufferedData[from] << packetInfo;
+                std::string totalReceivedData(totalStream.str());
+                NS_LOG_INFO("Node " << GetNode()->GetId() << " Total Received Data: " << totalReceivedData);
+
+                while ((pos = totalReceivedData.find(delimiter)) != std::string::npos) {
+                    parsedPacket = totalReceivedData.substr(0, pos);
+                    NS_LOG_INFO("Node " << GetNode()->GetId() << " Parsed Packet: " << parsedPacket);
+
+                    rapidjson::Document d;
+                    d.Parse(parsedPacket.c_str());
+
+                    if (!d.IsObject()) {
+                        NS_LOG_WARN("The parsed packet is corrupted");
+                        totalReceivedData.erase(0, pos + delimiter.length());
+                        continue;
+                    }
+
+                    rapidjson::StringBuffer buffer;
+                    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+                    d.Accept(writer);
+
+                    NS_LOG_INFO ("At time " << Simulator::Now().GetSeconds()
+                                            << "s iot node " << GetNode()->GetId() << " received "
+                                            << packet->GetSize() << " bytes from "
+                                            << Inet6SocketAddress::ConvertFrom(from).GetIpv6()
+                                            << " port " << Inet6SocketAddress::ConvertFrom(from).GetPort()
+                                            << " with info = " << buffer.GetString());
+
+
+                    totalReceivedData.erase(0, pos + delimiter.length());
+                }
+
+                /**
+                * Buffer the remaining data
+                */
+
+//                m_bufferedData[from] = totalReceivedData;
+                delete[] packetInfo;
+            } else if (Inet6SocketAddress::IsMatchingType(from)) {
+                NS_LOG_INFO ("At time " << Simulator::Now().GetSeconds()
+                                        << "s iot node " << GetNode()->GetId() << " received "
+                                        << packet->GetSize() << " bytes from "
+                                        << Inet6SocketAddress::ConvertFrom(from).GetIpv6()
+                                        << " port " << Inet6SocketAddress::ConvertFrom(from).GetPort());
+            }
+            m_rxTrace(packet, from);
+        }
+    }
+
+
+    void
+    IotNode::SendMessage(enum Messages receivedMessage, enum Messages responseMessage, std::string packet,
+                               Ipv4Address &outgoingIpv4Address) {
+        NS_LOG_FUNCTION (this);
+
+        const uint8_t delimiter[] = "#";
+
+        rapidjson::Document d;
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+
+        d.Parse(packet.c_str());
+        d["message"].SetInt(responseMessage);
+        d.Accept(writer);
+        NS_LOG_INFO("Node " << GetNode()->GetId() << " sent a " << getMessageName(responseMessage) << " message: "
+                            << buffer.GetString());
+
+        std::map<Ipv4Address, Ptr<Socket>>::iterator it = m_peersSockets.find(outgoingIpv4Address);
+
+        if (it == m_peersSockets.end()) {
+            m_peersSockets[outgoingIpv4Address] = Socket::CreateSocket(GetNode(), TcpSocketFactory::GetTypeId());
+//            m_peersSockets[outgoingIpv4Address]->Connect (Inet4SocketAddress (outgoingIpv4Address, m_commPort));
+        }
+        Ptr<Socket> outgoingSocket = m_peersSockets[outgoingIpv4Address];
+        outgoingSocket->Send(reinterpret_cast<const uint8_t *>(buffer.GetString()), buffer.GetSize(), 0);
+        outgoingSocket->Send(delimiter, 1, 0);
+    }
+
+    void
+    IotNode::SendMessage(enum Messages receivedMessage, enum Messages responseMessage, std::string packet,
+                               Ptr<Socket> outgoingSocket) {
+        NS_LOG_FUNCTION (this);
+
+        const uint8_t delimiter[] = "#";
+
+        rapidjson::Document d;
+        rapidjson::StringBuffer buffer;
+        rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+
+        d.Parse(packet.c_str());
+        d["message"].SetInt(responseMessage);
+        d.Accept(writer);
+        NS_LOG_INFO("Node " << GetNode()->GetId() << " sent a " << getMessageName(responseMessage) << " message: "
+                            << buffer.GetString());
+
+        outgoingSocket->Send(reinterpret_cast<const uint8_t *>(buffer.GetString()), buffer.GetSize(), 0);
+        outgoingSocket->Send(delimiter, 1, 0);
+    }
+
+    void
+    IotNode::HandlePeerClose(Ptr<Socket> socket) {
+        NS_LOG_FUNCTION (this << socket);
+    }
+
+    void IotNode::HandlePeerError(Ptr<Socket> socket) {
+        NS_LOG_FUNCTION (this << socket);
+    }
+
+    void
+    IotNode::HandleAccept(Ptr<Socket> s, const Address &from) {
+        NS_LOG_FUNCTION (this << s << from);
+        s->SetRecvCallback(MakeCallback(&IotNode::HandleRead, this));
+    }
+
+} //namespace ns3
--- src/internet-of-things/model/iot-node-application.h
+++ src/internet-of-things/model/iot-node-application.h
@@ -1,14 +1,239 @@
 //
-// Created by anyonepaw on 2019-04-20.
+// Created by anyonepaw on 2019-03-26.
 //
 
-#ifndef NS3_IOT_REALIZATION_IOTNODEAPPLICATION_H
-#define NS3_IOT_REALIZATION_IOT_NODE_APPLICATION_H
+#include "ns3/application.h"
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+#include "ns3/address.h"
+#include "ns3/ipv4-address.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-apps-module.h"
+#include "ns3/ipv4-static-routing-helper.h"
+#include "ns3/ipv4-routing-table-entry.h"
+#include "ns3/sixlowpan-module.h"
+#include "ns3/bitcoin.h"
+#include "../3rd-party/rapidjson/include/rapidjson/document.h"
+#include "../3rd-party/rapidjson/include/rapidjson/writer.h"
+#include "../3rd-party/rapidjson/include/rapidjson/stringbuffer.h"
+#include <random>
+
+namespace ns3 {
+
+    class Address;
+
+    class Socket;
+
+    class Packet;
+
+    class IotNode : public Application {
+    public:
+        /**
+         * \brief Get the type ID.
+         * \return the object TypeId
+         */
+        static TypeId GetTypeId(void);
+
+        IotNode();
 
 
-class iotnodeapplication {
+        virtual ~IotNode();
 
-};
+        /**
+         * \return pointer to listening socket
+         */
+        Ptr<Socket> GetListeningSocket(void) const;
+
+        /**
+         * \return a vector containing the addresses of peers
+         */
+        std::vector<Ipv4Address> GetPeersAddresses(void) const;
+
+        /**
+         * \return the node's public key
+         */
+//        CryptoPP::RSA::PublicKey GetPublickey(void) const;
+//
+//        /**
+//         * \return the node's public key
+//         */
+//        CryptoPP::RSA::PrivateKey GetPrivatekey(void) const;
+//
+//        /**
+//         * \return the node's gateway public key
+//         */
+//        CryptoPP::RSA::PublicKey GetGatewayPublickey(void) const;
+//
+//        /**
+//         * \brief Revoke and change the node's keys
+//         *	\param new private key and public key to be updated.
+//         */
+//        void SetNewKeys(CryptoPP::RSA::PrivateKey newPrivKey, CryptoPP::RSA::PublicKey newPublicKey);
+//
+//        /**
+//         * \brief Update the node's gateway public key
+//         *	\param new public key of the gateway
+//         */
+//        void SetGatewayPublickey(CryptoPP::RSA::PublicKey newGatewayKey);
+
+        /**
+         * \brief Set the addresses of peers
+         * \param peers the reference of a vector containing the Ipv6 addresses of peers
+         */
+        void SetPeersAddresses(const std::vector<Ipv4Address> &peers);
+
+        /**
+         * \brief adds a peer to the set of peers
+         * \param newPeer address of the peer to be added
+         **/
+        void AddPeer(Ipv4Address newPeer);
+
+        /**
+         * \brief set the download speeds of peers
+         * \param peersDownloadSpeeds the reference of a map containing the Ipv6 addresses of peers and their corresponding download speed
+         */
+        void SetPeersDownloadSpeeds(const std::map<Ipv4Address, double> &peersDownloadSpeeds);
+
+        /**
+         * \brief Set the upload speeds of peers
+         * \param peersUploadSpeeds the reference of a map containing the Ipv6 addresses of peers and their corresponding upload speed
+         */
+        void SetPeersUploadSpeeds(const std::map<Ipv4Address, double> &peersUploadSpeeds);
+
+        /**
+         * \brief Set the internet speeds of the node
+         * \param internetSpeeds a struct containing the download and upload speed of the node
+         */
+        void SetNodeInternetSpeeds(const nodeInternetSpeeds &internetSpeeds);
+
+        /**
+         * \brief Set the node statistics
+         * \param nodeStats a reference to a nodeStatistics struct
+         */
+        void SetNodeStats(nodeStatistics *nodeStats);
+
+        /**
+         * \brief Set the protocol type(default: STANDARD_PROTOCOL)
+         * \param protocolType the type of protocol used for advertising new blocks
+         */
+        void SetProtocolType(enum ProtocolType protocolType);
+
+        /**
+         * \brief Sends a message to a peer
+         * \param receivedMessage the type of the received message
+         * \param responseMessage the type of the response message
+         * \param d the rapidjson document containing the info of the outgoing message
+         * \param outgoingSocket the socket of the peer
+         */
+        void SendMessage(enum Messages receivedMessage, enum Messages responseMessage, rapidjson::Document &d,
+                         Ptr<Socket> outgoingSocket);
+
+        /**
+         * \brief Sends a message to a peer
+         * \param receivedMessage the type of the received message
+         * \param responseMessage the type of the response message
+         * \param d the rapidjson document containing the info of the outgoing message
+         * \param outgoingAddress the Address of the peer
+         */
+        void SendMessage(enum Messages receivedMessage, enum Messages responseMessage, rapidjson::Document &d,
+                         Address &outgoingAddress);
+
+        /**
+         * \brief Sends a message to a peer
+         * \param receivedMessage the type of the received message
+         * \param responseMessage the type of the response message
+         * \param packet a string containing the info of the outgoing message
+         * \param outgoingAddress the Address of the peer
+         */
+        void SendMessage(enum Messages receivedMessage, enum Messages responseMessage, std::string packet,
+                         Address &outgoingAddress);
+
+        /**
+         * \brief Sends a message to a peer
+         * \param receivedMessage the type of the received message
+         * \param responseMessage the type of the response message
+         * \param packet a string containing the info of the outgoing message
+         * \param outgoingIpv4Address the Ipv4Address of the peer
+         */
+        void SendMessage(enum Messages receivedMessage, enum Messages responseMessage, std::string packet,
+                         Ipv4Address &outgoingIpv4Address);
+
+        /**
+         * \brief Sends a message to a peer
+         * \param receivedMessage the type of the received message
+         * \param responseMessage the type of the response message
+         * \param packet a string containing the info of the outgoing message
+         * \param outgoingSocket the socket of the peer
+         */
+        void SendMessage(enum Messages receivedMessage, enum Messages responseMessage, std::string packet,
+                         Ptr<Socket> outgoingSocket);
+
+    protected:
+        // inherited from Application base class.
+        virtual void StartApplication(void); // Called at time specified by Start
+        virtual void StopApplication(void); // Called at time specified by Stop
+
+        virtual void DoDispose(void);
+
+        /**
+         * \brief Handle a packet received by the application
+         * \param socket the receiving socket
+         */
+        void HandleRead(Ptr<Socket> socket);
+
+        /**
+         * \brief Handle an incoming connection
+         * \param socket the incoming connection socket
+         * \param from the address the connection is from
+         */
+        void HandleAccept(Ptr<Socket> socket, const Address &from);
+
+        /**
+         * \brief Handle an connection close
+         * \param socket the connected socket
+         */
+        void HandlePeerClose(Ptr<Socket> socket);
+
+        /**
+         * \brief Handle an connection error
+         * \param socket the connected socket
+         */
+        void HandlePeerError(Ptr<Socket> socket);
+
+    private:
+
+        // In the case of TCP, each socket accept returns a new socket, so the
+        // listening socket is stored separately from the accepted sockets
+        Ptr<Socket> m_socket;                    //!< Listening socket
+        Address m_local;                              //!< Local address to bind to
+        TypeId m_tid;                                 //!< Protocol TypeId
+        int m_numberOfPeers;                          //!< Number of node's peers
+        int m_gatewayNodeId;             //!< Node Id for the gateway
+        Ipv4Address m_gatewayAddress;  //!< Ipv6 address of the gateway
+        double m_downloadSpeed;                       //!< The download speed of the node in Bytes/s
+        double m_uploadSpeed;                         //!< The upload speed of the node in Bytes/s
+
+        std::vector<Ipv4Address> m_peersAddresses;           //!< The addresses of peers
+        std::map<Ipv4Address, double> m_peersDownloadSpeeds;      //!< The peersDownloadSpeeds of channels
+        std::map<Ipv4Address, double> m_peersUploadSpeeds;        //!< The peersUploadSpeeds of channels
+
+        std::map<Ipv4Address, Ptr<Socket> > m_peersSockets;            //!< The sockets of peers
+        nodeStatistics *m_nodeStats;                //!< struct holding the node stats
+        std::map<Ipv4Address, std::vector<std::string> > m_messages; //!< map holding all the messages from different peers
+        enum ProtocolType m_protocolType;                                               //!< protocol type
+
+        const int m_commPort;               //!< 5555
+        const int m_secondsPerMin;             //!< 60
+
+        /// Traced Callback: received packets, source address.
+        TracedCallback<Ptr<const Packet>, const Address &> m_rxTrace;
 
 
-#endif //NS3_IOT_REALIZATION_IOTNODEAPPLICATION_H
+    };
+
+
+} // namespace ns3
+
+#endif
