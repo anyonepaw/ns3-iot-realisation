bb0788ac8e7eb7217542099b2b6a8f15cb311357
IlyaWhitee <jipbel@mail.ru>
2019-05-27 15:42:51 +0300

    Архитектура с 5 узлами

--- scratch/one-iot.cc
+++ scratch/one-iot.cc
@@ -19,10 +19,15 @@
 #include "ns3/csma-module.h"
 #include "ns3/bridge-helper.h"
 
+
+
+
+
 using namespace ns3;
 using namespace std;
 
-NS_LOG_COMPONENT_DEFINE ("Trying_toBuild_NAT");
+NS_LOG_COMPONENT_DEFINE ("one-iot_with_sniffer");
+
 
 void AddMobility(double val1, double val2, NodeContainer container);
 
@@ -39,8 +44,12 @@ main (int argc, char *argv[])
 
     //первые 2 узла
 
+    NodeContainer Client_Router;
+    Client_Router.Create(2);
+
     NodeContainer firstNodeContainer;
-    firstNodeContainer.Create (2);
+    firstNodeContainer.Add (Client_Router.Get(1));
+    firstNodeContainer.Create(1);
 
     //вторые 2 узла
 
@@ -48,20 +57,34 @@ main (int argc, char *argv[])
     secondNodeContainer.Add ( firstNodeContainer.Get (1) );
     secondNodeContainer.Create (1);
 
+    NodeContainer AttackNodeContainer;
+    AttackNodeContainer.Add ( firstNodeContainer.Get (1) );
+    AttackNodeContainer.Create (1);
+
+    NodeContainer AttackNodeContainer1;
+    AttackNodeContainer1.Add ( Client_Router.Get (0) );
+    AttackNodeContainer1.Add (AttackNodeContainer.Get(1));
+
+
     //именуем узлы
 
-    Names::Add("Client", firstNodeContainer.Get(0));
+    Names::Add("Server", Client_Router.Get(0));
+    Names::Add("Router", Client_Router.Get(1));
     Names::Add("Gate", firstNodeContainer.Get(1));
-    Names::Add("Server", secondNodeContainer.Get(1));
+    Names::Add("IoT", secondNodeContainer.Get(1));
+    Names::Add("Attack_router", AttackNodeContainer.Get(1));
 
     /** MOBILITY */ //задаем координаты узлам
 
     /** NODES*/
-    AddMobility(20.0, 0.0, firstNodeContainer.Get(0));
+    AddMobility(20.0, - 5.0, Client_Router.Get(0));
+    AddMobility(20.0, 0, Client_Router.Get(1));
     AddMobility(20.0, 10.0, firstNodeContainer.Get(1));
     AddMobility(25.0, 15.0, secondNodeContainer.Get(1));
+    AddMobility(15.0, 0, AttackNodeContainer.Get(1));
 
     //строим топологии для двух пар узлов
+
 
     PointToPointHelper p2p1;
     p2p1.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
@@ -71,16 +94,37 @@ main (int argc, char *argv[])
     p2p2.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
     p2p2.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
+    PointToPointHelper p2p3;
+    p2p3.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    p2p3.SetChannelAttribute("Delay", StringValue("1ms"));
+
+    PointToPointHelper p2p4;
+    p2p4.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    p2p4.SetChannelAttribute("Delay", StringValue("1ms"));
+
+    PointToPointHelper p2p5;
+    p2p5.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    p2p5.SetChannelAttribute("Delay", StringValue("1ms"));
+
+
+
     //подстраиваем топологии к узлам
 
-    NetDeviceContainer devices = p2p1.Install (firstNodeContainer);
-    NetDeviceContainer devices2 = p2p2.Install (secondNodeContainer);
+    NetDeviceContainer devices = p2p1.Install (Client_Router);
+    NetDeviceContainer devices2 = p2p2.Install (firstNodeContainer);
+    NetDeviceContainer devices3 = p2p3.Install (secondNodeContainer);
+    NetDeviceContainer devices4 = p2p4.Install (AttackNodeContainer);
+    NetDeviceContainer devices5 = p2p5.Install (AttackNodeContainer1);
 
     // создаем и устанавливаем все сетевые карты, учавсвтующие в передаче
 
     InternetStackHelper internet;
-    internet.Install (firstNodeContainer);
-    internet.Install (secondNodeContainer.Get (1));
+    internet.Install (Client_Router);
+    internet.Install (firstNodeContainer.Get (1));
+    internet.Install (secondNodeContainer.Get(1));
+    internet.Install (AttackNodeContainer.Get (1));
+
+
 
     // устанавливаем базовый IP адресс и маску для первых и вторых пар узлов
 
@@ -88,12 +132,25 @@ main (int argc, char *argv[])
     address1.SetBase ("192.168.1.0", "255.255.255.0");
 
     Ipv4AddressHelper address2;
-    address2.SetBase ("203.82.48.0", "255.255.255.0");
+    address2.SetBase ("192.168.2.0", "255.255.255.0");
+
+    Ipv4AddressHelper address3;
+    address3.SetBase ("192.168.3.0", "255.255.255.0");
+
+    Ipv4AddressHelper address4;
+    address4.SetBase ("203.82.48.0", "255.255.255.0");
+
+    Ipv4AddressHelper address5;
+    address5.SetBase ("203.82.49.0", "255.255.255.0");
 
     // присваеваем IP адресс нашим устройствам
 
     Ipv4InterfaceContainer firstInterfaces = address1.Assign (devices);
     Ipv4InterfaceContainer secondInterfaces = address2.Assign (devices2);
+    Ipv4InterfaceContainer thirdInterfaces = address3.Assign (devices3);
+    Ipv4InterfaceContainer attackInterfaces = address4.Assign (devices4);
+    Ipv4InterfaceContainer attackInterfaces1 = address5.Assign (devices5);
+
 
 
 //NAT
@@ -130,12 +187,23 @@ main (int argc, char *argv[])
     // Протокол TCP: клиент-серверное сообщение
     NS_LOG_UNCOND ("TCP trace:");
 
-    TcpEchoClientHelper tcpClient(secondInterfaces.GetAddress (1), 9);
+
+    TcpEchoClientHelper tcpClient(attackInterfaces.GetAddress (0), 9);
     tcpClient.SetAttribute ("MaxPackets", UintegerValue (1));
     tcpClient.SetAttribute ("Interval", TimeValue (Seconds (1.)));
     tcpClient.SetAttribute ("PacketSize", UintegerValue (512));
 
-    ApplicationContainer client1 = tcpClient.Install (firstNodeContainer.Get (0));
+    ApplicationContainer client = tcpClient.Install (Client_Router.Get (0));
+    client.Start(Seconds (2.0));
+    client.Stop (Seconds (12.0));
+
+
+    TcpEchoClientHelper tcpClient1(thirdInterfaces.GetAddress (1), 9);
+    tcpClient1.SetAttribute ("MaxPackets", UintegerValue (1));
+    tcpClient1.SetAttribute ("Interval", TimeValue (Seconds (1.)));
+    tcpClient1.SetAttribute ("PacketSize", UintegerValue (512));
+
+    ApplicationContainer client1 = tcpClient1.Install (Client_Router.Get (0));
     client1.Start(Seconds (2.0));
     client1.Stop (Seconds (10.0));
 
@@ -145,12 +213,18 @@ main (int argc, char *argv[])
     server.Stop (Seconds (10.0));
 
 
-    /* PCAP */
-    p2p2.EnablePcapAll ("ipv4-nat", false);
-    p2p1.EnablePcapAll ("ipv4-nat", false);
 
-    p2p1.EnablePcapAll("p2p1");
-    p2p2.EnablePcapAll("p2p2");
+    //AsciiTraceHelper ascii;
+   // p2p2.EnableAsciiAll (ascii.CreateFileStream ("p2p2.tr"));
+    //p2p1.EnableAsciiAll (ascii.CreateFileStream ("p2p1.tr"));
+
+
+    /* PCAP */
+  //  p2p2.EnablePcapAll ("ipv4-nat", false);
+   // p2p1.EnablePcapAll ("ipv4-nat", false);
+
+   // p2p1.EnablePcapAll("p2p1");
+   // p2p2.EnablePcapAll("p2p2");
 
 
     /* ANIMATION */
---
+++ scratch/one-iot_and_traffic-control.cc
@@ -0,0 +1,4 @@
+//
+// Created by ricardo on 23.05.19.
+//
+
