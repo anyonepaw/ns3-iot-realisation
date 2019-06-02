02353ae9f1c8b4dc186a63b1e2f260a91aaa0447
Anzhelika Pokhodun <an.pokhodun@gmail.com>
2019-06-02 15:19:52 +0300

    Done with multiple-iot model

--- scratch/multiple-iot.cc
+++ scratch/multiple-iot.cc
@@ -4,6 +4,8 @@
 
 #include <utility>
 
+#include <utility>
+
 #include "ns3/core-module.h"
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
@@ -36,8 +38,9 @@ NS_LOG_COMPONENT_DEFINE ("ScratchSimulator");
 
 const InternetStackHelper STACK;
 Ipv4AddressHelper ADDRESS_IPV4;
-const uint32_t NUMBER_OF_IOT_NETWORKS = 8;
+const uint32_t NUMBER_OF_IOT_NETWORKS = 7;
 
+void addP2P(NodeContainer node1, NodeContainer node2);
 
 void addMobility(double x_position, double y_position, NodeContainer container);
 
@@ -53,15 +56,14 @@ main(int argc, char *argv[]) {
     NodeContainer gateNodes;
     gateNodes.Create(NUMBER_OF_IOT_NETWORKS);
 
-//    NodeContainer routerNodes;
-//    routerNodes.Create(NUMBER_OF_IOT_NETWORKS);
-
-
     NodeContainer clientNode;
     clientNode.Create(1);
     Ptr<Node> client = clientNode.Get(0);
 
     /** IOT */
+    NodeContainer vlan0Nodes;
+    vlan0Nodes.Create(3);
+
     NodeContainer vlan1Nodes;
     vlan1Nodes.Create(2);
 
@@ -70,6 +72,19 @@ main(int argc, char *argv[]) {
 
     NodeContainer vlan3Nodes;
     vlan3Nodes.Create(3);
+
+    NodeContainer vlan4Nodes;
+    vlan4Nodes.Create(2);
+
+    NodeContainer vlan5Nodes;
+    vlan5Nodes.Create(2);
+
+    NodeContainer vlan6Nodes;
+    vlan6Nodes.Create(3);
+
+//    NodeContainer attackerNode;
+//    attackerNode.Create(1);
+//    Ptr<Node> attacker = attackerNode.Get(0);
 
 
     NS_LOG_UNCOND ("Router and internet-things created");
@@ -80,11 +95,11 @@ main(int argc, char *argv[]) {
         Names::Add(oss.str(), gateNodes.Get(i));
     }
 
-    for (uint32_t i = 0; i < vlan1Nodes.GetN(); ++i) {
-        std::ostringstream oss;
-        oss << "Vlan1IoT" << i;
-        Names::Add(oss.str(), vlan1Nodes.Get(i));
-    }
+//    for (uint32_t i = 0; i < vlan1Nodes.GetN(); ++i) {
+//        std::ostringstream oss;
+//        oss << "Vlan1IoT" << i;
+//        Names::Add(oss.str(), vlan1Nodes.Get(i));
+//    }
 
     Names::Add("Client", client);
 
@@ -94,47 +109,153 @@ main(int argc, char *argv[]) {
 
     /** GATES */
     addMobility(20.0, 0.0, gateNodes.Get(0));
-    addMobility(5.0, 20.0, gateNodes.Get(1));
-    addMobility(35.0, 20.0, gateNodes.Get(2));
+    addMobility(0.0, 20.0, gateNodes.Get(1));
+    addMobility(0.0, 40.0, gateNodes.Get(2));
+    addMobility(20.0, 60.0, gateNodes.Get(3));
+    addMobility(40.0, 60.0, gateNodes.Get(4));
+    addMobility(60.0, 40.0, gateNodes.Get(5));
+    addMobility(60.0, 20.0, gateNodes.Get(6));
 
+    addMobility(40.0, 0.0, client);
 
-    addMobility(60.0, 0.0, client);
 
     // addMobility(26.0, 0.0, routerNodes.Get(0));
 
     /** VLANS: IOT*/
-    addMobility(0.0, -10.0, vlan1Nodes.Get(0));
-    addMobility(0.0, -20.0, vlan1Nodes.Get(1));
-    addMobility(-10.0, 25.0, vlan2Nodes.Get(0));
-    addMobility(0.0, 30.0, vlan2Nodes.Get(1));
-    addMobility(60.0, 30.0, vlan3Nodes.Get(0));
+    addMobility(-10.0, -5.0, vlan0Nodes.Get(0));
+    addMobility(-10.0, -20.0, vlan0Nodes.Get(1));
+    addMobility(3.0, -25.0, vlan0Nodes.Get(2));
+
+    addMobility(-20.0, 15.0, vlan1Nodes.Get(0));
+    addMobility(-20.0, 25.0, vlan1Nodes.Get(1));
+
+    addMobility(-20.0, 35.0, vlan2Nodes.Get(0));
+    addMobility(-20.0, 45.0, vlan2Nodes.Get(1));
+
+    addMobility(10.0, 80.0, vlan3Nodes.Get(0));
+    addMobility(20.0, 80.0, vlan3Nodes.Get(1));
+    addMobility(30.0, 80.0, vlan3Nodes.Get(2));
+
+    addMobility(35.0, 80.0, vlan4Nodes.Get(0));
+    addMobility(45.0, 80.0, vlan4Nodes.Get(1));
+
+    addMobility(80.0, 35.0, vlan5Nodes.Get(0));
+    addMobility(80.0, 45.0, vlan5Nodes.Get(1));
+
+    addMobility(80.0, 5.0, vlan6Nodes.Get(0));
+    addMobility(80.0, 15.0, vlan6Nodes.Get(1));
+    addMobility(80.0, 25.0, vlan6Nodes.Get(2));
+
+//    addMobility(80.0, 80.0, attacker);
 
 
+    addP2P(gateNodes.Get(0),gateNodes.Get(1));
+    addP2P(gateNodes.Get(0),gateNodes.Get(2));
+    addP2P(gateNodes.Get(0),gateNodes.Get(3));
+    addP2P(gateNodes.Get(0),gateNodes.Get(4));
+    addP2P(gateNodes.Get(0),gateNodes.Get(5));
+    addP2P(gateNodes.Get(0),gateNodes.Get(6));
+    addP2P(gateNodes.Get(0),client);
+
+    addP2P(gateNodes.Get(0),vlan0Nodes.Get(0));
+    addP2P(gateNodes.Get(0),vlan0Nodes.Get(1));
+    addP2P(gateNodes.Get(0),vlan0Nodes.Get(2));
+
+    addP2P(gateNodes.Get(1),gateNodes.Get(0));
+    addP2P(gateNodes.Get(1),gateNodes.Get(2));
+    addP2P(gateNodes.Get(1),gateNodes.Get(3));
+    addP2P(gateNodes.Get(1),gateNodes.Get(4));
+    addP2P(gateNodes.Get(1),gateNodes.Get(5));
+    addP2P(gateNodes.Get(1),gateNodes.Get(6));
+    addP2P(gateNodes.Get(1),client);
+
+    addP2P(gateNodes.Get(1),vlan1Nodes.Get(0));
+    addP2P(gateNodes.Get(1),vlan1Nodes.Get(1));
+
+    addP2P(gateNodes.Get(2),gateNodes.Get(0));
+    addP2P(gateNodes.Get(2),gateNodes.Get(1));
+    addP2P(gateNodes.Get(2),gateNodes.Get(3));
+    addP2P(gateNodes.Get(2),gateNodes.Get(4));
+    addP2P(gateNodes.Get(2),gateNodes.Get(5));
+    addP2P(gateNodes.Get(2),gateNodes.Get(6));
+    addP2P(gateNodes.Get(2),client);
+
+    addP2P(gateNodes.Get(2),vlan2Nodes.Get(0));
+    addP2P(gateNodes.Get(2),vlan2Nodes.Get(1));
+
+    addP2P(gateNodes.Get(3),gateNodes.Get(0));
+    addP2P(gateNodes.Get(3),gateNodes.Get(1));
+    addP2P(gateNodes.Get(3),gateNodes.Get(2));
+    addP2P(gateNodes.Get(3),gateNodes.Get(4));
+    addP2P(gateNodes.Get(3),gateNodes.Get(5));
+    addP2P(gateNodes.Get(3),gateNodes.Get(6));
+    addP2P(gateNodes.Get(3),client);
+
+    addP2P(gateNodes.Get(3),vlan3Nodes.Get(0));
+    addP2P(gateNodes.Get(3),vlan3Nodes.Get(1));
+    addP2P(gateNodes.Get(3),vlan3Nodes.Get(2));
+
+    addP2P(gateNodes.Get(4),gateNodes.Get(0));
+    addP2P(gateNodes.Get(4),gateNodes.Get(1));
+    addP2P(gateNodes.Get(4),gateNodes.Get(2));
+    addP2P(gateNodes.Get(4),gateNodes.Get(3));
+    addP2P(gateNodes.Get(4),gateNodes.Get(5));
+    addP2P(gateNodes.Get(4),gateNodes.Get(6));
+    addP2P(gateNodes.Get(4),client);
+
+    addP2P(gateNodes.Get(4),vlan4Nodes.Get(0));
+    addP2P(gateNodes.Get(4),vlan4Nodes.Get(1));
 
 
-//        private address    NAT      public address
-// n0 <--------------------> n1 <-----------------------> n2
-// 192.168.1.1      192.168.1.2 203.82.48.1          203.82.48.2
-//
-//    Ipv4NatHelper natHelper;
-//    Ptr<Ipv4Nat> nat = natHelper.Install(gateNodes.Get(0));
-//
-//    nat->SetInside(1);
-//    nat->SetOutside(2);
-//
-//    //Adding the address to be translated to and port pools.
-//
-//    nat->AddAddressPool(Ipv4Address("192.168.1.5"), Ipv4Mask("255.255.255.255"));
-//    nat->AddPortPool(49153, 49163);
-//    // Add a rule here to map outbound connections from n0, port 49153, UDP
-//    Ipv4DynamicNatRule rule(Ipv4Address("192.168.1.0"), Ipv4Mask("255.255.255.0"));
-//    nat->AddDynamicRule(rule);
+    addP2P(gateNodes.Get(5),gateNodes.Get(0));
+    addP2P(gateNodes.Get(5),gateNodes.Get(1));
+    addP2P(gateNodes.Get(5),gateNodes.Get(2));
+    addP2P(gateNodes.Get(5),gateNodes.Get(3));
+    addP2P(gateNodes.Get(5),gateNodes.Get(4));
+    addP2P(gateNodes.Get(5),gateNodes.Get(6));
+    addP2P(gateNodes.Get(5),client);
 
-    PointToPointHelper p2p1;
-    p2p1.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    p2p1.SetChannelAttribute("Delay", StringValue("1ms"));
+    addP2P(gateNodes.Get(5),vlan5Nodes.Get(0));
+    addP2P(gateNodes.Get(5),vlan5Nodes.Get(1));
 
 
+    addP2P(gateNodes.Get(6),gateNodes.Get(0));
+    addP2P(gateNodes.Get(6),gateNodes.Get(1));
+    addP2P(gateNodes.Get(6),gateNodes.Get(2));
+    addP2P(gateNodes.Get(6),gateNodes.Get(3));
+    addP2P(gateNodes.Get(6),gateNodes.Get(4));
+    addP2P(gateNodes.Get(6),gateNodes.Get(5));
+    addP2P(gateNodes.Get(6),client);
+
+    addP2P(gateNodes.Get(6),vlan6Nodes.Get(0));
+    addP2P(gateNodes.Get(6),vlan6Nodes.Get(1));
+    addP2P(gateNodes.Get(6),vlan6Nodes.Get(2));
+
+    for (int j = 0; j < NUMBER_OF_IOT_NETWORKS; ++j) {
+        addP2P(client,gateNodes.Get(j));
+    }
+
+//    addP2P(attacker,gateNodes.Get(0));
+//    addP2P(attacker,gateNodes.Get(1));
+//    addP2P(attacker,gateNodes.Get(2));
+//    addP2P(attacker,gateNodes.Get(3));
+//    addP2P(attacker,gateNodes.Get(4));
+//    addP2P(attacker,gateNodes.Get(5));
+//    addP2P(attacker,gateNodes.Get(6));
+//    addP2P(attacker,client);
+
+    InternetStackHelper internet;
+    internet.Install(gateNodes.Get(0));
+    internet.Install(gateNodes.Get(1));
+    internet.Install(gateNodes.Get(2));
+    internet.Install(gateNodes.Get(3));
+    internet.Install(gateNodes.Get(4));
+    internet.Install(gateNodes.Get(5));
+    internet.Install(gateNodes.Get(6));
+    internet.Install(client);
+
+    Ipv4AddressHelper address1;
+    address1.SetBase("234.134.67.68", "255.255.255.0");
 
 //
 //    for (int j = 0; j < vlan1IoTsInterfaces.GetN(); ++j) {
@@ -169,31 +290,49 @@ main(int argc, char *argv[]) {
 
     string sourceDir = ICONS_DIR;
 
-    uint32_t resourceId1 = anim.AddResource(sourceDir + "/iot.png");
+    uint32_t pictureIoT = anim.AddResource(sourceDir + "/iot.png");
     uint32_t pictureGate = anim.AddResource(sourceDir + "/gate.png");
-    uint32_t pictureRouter = anim.AddResource(sourceDir + "/router.png");
     uint32_t pictureClient = anim.AddResource(sourceDir + "/client.png");
+    uint32_t pictureAttacker = anim.AddResource(sourceDir + "/attacker.png");
 
     //anim.EnableIpv4RouteTracking()
 
-    anim.UpdateNodeImage(0, pictureGate);
+    anim.UpdateNodeImage(0, pictureGate); // "0" is the Node ID (from Mobility)
     anim.UpdateNodeImage(1, pictureGate);
     anim.UpdateNodeImage(2, pictureGate);
-    anim.UpdateNodeImage(4, resourceId1);  // "0" is the Node ID
-    anim.UpdateNodeImage(5, resourceId1);
-    anim.UpdateNodeImage(6, resourceId1);
-    anim.UpdateNodeImage(7, resourceId1);
-    anim.UpdateNodeImage(3, pictureClient);
-    anim.UpdateNodeImage(8, resourceId1);
+    anim.UpdateNodeImage(3, pictureGate);
+    anim.UpdateNodeImage(4, pictureGate);
+    anim.UpdateNodeImage(5, pictureGate);
+    anim.UpdateNodeImage(6, pictureGate);
+    anim.UpdateNodeImage(7, pictureClient);
+
+    anim.UpdateNodeImage(8, pictureIoT);
+    anim.UpdateNodeImage(9, pictureIoT);
+    anim.UpdateNodeImage(10, pictureIoT);
+    anim.UpdateNodeImage(11, pictureIoT);
+    anim.UpdateNodeImage(12, pictureIoT);
+    anim.UpdateNodeImage(13, pictureIoT);
+    anim.UpdateNodeImage(14, pictureIoT);
+    anim.UpdateNodeImage(15, pictureIoT);
+    anim.UpdateNodeImage(16, pictureIoT);
+    anim.UpdateNodeImage(17, pictureIoT);
+    anim.UpdateNodeImage(18, pictureIoT);
+    anim.UpdateNodeImage(19, pictureIoT);
+    anim.UpdateNodeImage(20, pictureIoT);
+    anim.UpdateNodeImage(21, pictureIoT);
+    anim.UpdateNodeImage(22, pictureIoT);
+    anim.UpdateNodeImage(23, pictureIoT);
+    anim.UpdateNodeImage(24, pictureIoT);
+
+//    anim.UpdateNodeImage(25, pictureAttacker);
 
 
-    for (uint32_t i = 0; i <= 8; i++) {
+
+    for (uint32_t i = 0; i <= 24; i++) {
         anim.UpdateNodeSize(i, 9, 9);
     }
 
     // anim.UpdateNodeCounter (89, 7, 3.4);
-
-
 
     // Simulator::Schedule (Seconds (0.1), modify);
 
@@ -203,6 +342,19 @@ main(int argc, char *argv[]) {
     Simulator::Destroy();
 
 }
+
+void addP2P(NodeContainer node1, NodeContainer node2) {
+    NodeContainer nodeContainer;
+    nodeContainer.Add(std::move(node1));
+    nodeContainer.Add(std::move(node2));
+
+    PointToPointHelper p2p1;
+    p2p1.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    p2p1.SetChannelAttribute("Delay", StringValue("1ms"));
+
+    NetDeviceContainer devices1 = p2p1.Install(nodeContainer);
+}
+
 
 void addMobility(double x_position, double y_position, NodeContainer container) {
     MobilityHelper mobility;
